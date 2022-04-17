7bae729c4f656f5fb368362ebc9b8acfeae4425f
Brian Pugh <bnp117@gmail.com>
2022-04-17 10:42:14 -0700

    repo naming prep

--- torchsample/__meta__.py
+++ torchsample/__meta__.py
@@ -1,12 +1,10 @@
-# `name` is the name of the package as used for `pip install package`
-name = "package_name"
-# `path` is the name of the package for `import package`
+name = "torchsample"
 path = name.lower().replace("-", "_").replace(" ", "_")
 # Your version number should follow https://python.org/dev/peps/pep-0440 and
 # https://semver.org
 version = "0.0.0"
 author = "Brian Pugh"
 author_email = "bnp117@gmail.com"
-description = ""  # One-liner
-url = "https://github.com/BrianPugh/package_name"
+description = "Neural network sampling utilities."
+url = "https://github.com/BrianPugh/torchsample"
 license = "apache-2.0"
