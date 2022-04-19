8827d373fcc0eaf026057d7868297943f3509ac2
Brian Pugh <bnp117@gmail.com>
2022-04-19 08:21:23 -0700

    initial commit test_sample

---
+++ tests/test_sample.py
@@ -0,0 +1,49 @@
+import torch
+
+import torchsample as ts
+
+
+def test_sample2d_coords_shape3():
+    coords = torch.tensor(
+        [
+            # Top Row
+            [-1.0, -1.0],
+            [0.0, -1.0],
+            [1.0, -1.0],
+            # Bottom Row
+            [-1.0, 1.0],
+            [0.0, 1.0],
+            [1.0, 1.0],
+            # Trying out small interpolations.
+            [-0.9, -1],
+        ]
+    )[None]
+    n_coords = coords.shape[1]
+
+    featmap = torch.tensor([[10.0, 20.0], [30.0, 40.0]])[None, None]
+    featmap = featmap.repeat(1, 5, 1, 1)
+
+    actual = ts.sample2d(coords, featmap)
+    assert actual.shape == (1, n_coords, 5)
+
+    # Top Row
+    # [-1, -1]: top-left pixel
+    # This in unnormalized coorinates gets mapped to (-0.5, -0.5).
+    # If ``align_corners=True, padding_mode="zeros"``,
+    # then the interpolated value is ``2.5``.
+    assert torch.allclose(actual[0, 0], torch.tensor(10.0))
+    # [0, -1]: middle between 10 and 20
+    assert torch.allclose(actual[0, 1], torch.tensor(15.0))
+    # [1, -1]: top-right pixel
+    assert torch.allclose(actual[0, 2], torch.tensor(20.0))
+
+    # Bottom Row
+    # [-1, 1]: bottom-left pixel
+    assert torch.allclose(actual[0, 3], torch.tensor(30.0))
+    # [0, 1]: middle between 30 and 40
+    assert torch.allclose(actual[0, 4], torch.tensor(35.0))
+    # [0, 1]: bottom-right pixel
+    assert torch.allclose(actual[0, 5], torch.tensor(40.0))
+
+    # Trying out small interpolations.
+    assert torch.allclose(actual[0, 6], torch.tensor(0.95 * 10 + 0.05 * 20))
