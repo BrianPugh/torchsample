e43abb0d50b3296cad85e364173fe99059137f24
Brian Pugh <bnp117@gmail.com>
2022-04-21 09:32:01 -0700

    models cuda tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 import torchtest
+from markers import cuda
 
 import torchsample as ts
 
@@ -17,4 +18,20 @@ def test_mlp():
         optim=torch.optim.Adam(model.parameters()),
         batch=batch,
         device="cpu",
+    )
+
+
+@cuda
+def test_mlp_cuda():
+    batch = [
+        torch.rand(7, 4096, 10),
+        torch.rand(7, 4096, 3),
+    ]
+    model = ts.models.MLP([10, 100, 100, 3])
+    torchtest.assert_vars_change(
+        model=model,
+        loss_fn=F.l1_loss,
+        optim=torch.optim.Adam(model.parameters()),
+        batch=batch,
+        device="cuda",
     )
